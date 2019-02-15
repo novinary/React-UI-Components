f67e9cd52a4900eb350f57fa226091f639e94f1a
novinary <novina.dev@gmail.com>
2019-02-15 08:53:16 +0000

    Worked on CardBanner.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import './Card.css';
+import CardBannerImage from './CardBannerImage';
+import CardBannerDescription from './CardBannerDescription';
 
 function CardBanner ({children}) {
-    return (
-      <div className="card-banner-container">
-        <div className="logo"></div>
-        {children}
-      </div>
+  return(
+    <div className="card-banner-container">
+        <div className="card-banner-img">
+            <CardBannerImage />
+        </div>
+        <div className="card-banner-description">
+            <CardBannerDescription />
+        </div>
+    </div>
     )
   }
   
