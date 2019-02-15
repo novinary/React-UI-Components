bfab2a60e13ef82ebe80e38336184880a18e8a95
novinary <novina.dev@gmail.com>
2019-02-15 10:47:42 +0000

    Code cleanup

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
-function CardBanner (props) {
-    return(
-        <React.Fragment>
-            <img className='ContentImg' src="https://reactjs.org/logo-og.png" alt='react logo'/>
-        </React.Fragment>
-    )
+function CardBanner(props) {
+  return (
+    <React.Fragment>
+      <img
+        className="ContentImg"
+        src="https://reactjs.org/logo-og.png"
+        alt="react logo"
+      />
+    </React.Fragment>
+  );
 }
-
 
 export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import HeaderContent from './HeaderContent';
-import HeaderTitle from './HeaderTitle';
+import React from "react";
+import HeaderContent from "./HeaderContent";
+import HeaderTitle from "./HeaderTitle";
 
-import './Header.css';
+import "./Header.css";
 
-
-function HeaderContainer (){
-    return(
-        <div className='header-container'>
-            <HeaderTitle />
-            <HeaderContent />  
-        </div>
-    )
+function HeaderContainer() {
+  return (
+    <div className="header-container">
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  );
 }
-
 
 export default HeaderContainer;
