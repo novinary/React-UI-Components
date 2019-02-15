85103dc3eae86b3c5feeabbb52373ccbad6b5096
novinary <novina.dev@gmail.com>
2019-02-15 13:48:08 +0000

    Working on the NumberButton function

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Button.css';
+import Button from './Button'
+
+const numbers = [1,2,3,4,5,6,7,8,9,0]
+const numberArr = numbers.map(x => <Button item = {x}/>)
 
 function NumberButton(props){
     return(
