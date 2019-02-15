fb72a15911166fce5425873d9c0488fcb9592648
novinary <novina.dev@gmail.com>
2019-02-15 13:44:52 +0000

    Imported ActionButton

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
       <NumberButton />
+      <ActionButton />
     </div>
   );
 };
