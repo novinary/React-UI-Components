cf44321870c2893dbaa770a24a394d7eba668389
novinary <novina.dev@gmail.com>
2019-02-15 14:04:32 +0000

    Moved 0 at the bottom

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Button.css';
 import Button from './Button'
 
-const numbers = [1,2,3,4,5,6,7,8,9,0]
+const numbers = [1,2,3,4,5,6,7,8,9]
 const numberArr = numbers.map(x => <Button item = {x}/>)
 
 function NumberButton(props){
     return(
         <div className="numbers">
             {numberArr}
+            <Button item = '0'/>
         </div>
     );
 }
