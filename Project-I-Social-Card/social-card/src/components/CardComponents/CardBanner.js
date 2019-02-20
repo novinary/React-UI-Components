62a1f43ba7bbf82023ea275e548a6ef5e7f465e0
Samarvir Singh <samarvir1996@gmail.com>
2019-02-20 11:54:34 +0530

    Revert "Novina Pun PR for project 1"

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,16 +1,2 @@
-import React from "react";
-import "./Card.css";
-
-function CardBanner(props) {
-  return (
-    <React.Fragment>
-      <img
-        className="ContentImg"
-        src="https://reactjs.org/logo-og.png"
-        alt="react logo"
-      />
-    </React.Fragment>
-  );
-}
-
-export default CardBanner;
+import React from 'react';
+import './Card.css';
